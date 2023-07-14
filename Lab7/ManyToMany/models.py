3a7841b48026d5250b786c1315491027768f231e
Alejandro Phocco <aphoccot@unsa.edu.pe>
2023-07-14 13:21:22 -0500

    Se agrego los modelos de muchos a muchos

--- Lab7/ManyToMany/models.py
+++ Lab7/ManyToMany/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Class(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+    
+class Student(models.Model):
+    name = models.CharField(max_length=10)
+    language = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
