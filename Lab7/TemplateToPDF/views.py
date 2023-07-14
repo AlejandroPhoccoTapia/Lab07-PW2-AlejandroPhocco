60f6938b9e51e97d0177d68e2a8d1c4f478e8d54
Alejandro Phocco <aphoccot@unsa.edu.pe>
2023-07-14 13:07:01 -0500

    Se creo la clase que ayudara a crear los pdfs y se creo el apartado utils.py que nos servira para renderizar los pdfs

--- Lab7/TemplateToPDF/views.py
+++ Lab7/TemplateToPDF/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.views.generic import View
+from .utils import render_to_pdf
+
 # Create your views here.
 
-def send(request):
-    return render(request, 'send.html')
+class GeneratePDF(View):
+    def get(self, request, *args, **kwargs):
+        template = get_template('pdf.html')
+        context = {
+            'name': 'Alejandro',
+            'lab': 7,
+        }
+        html = template.render(context)
+        pdf = render_to_pdf('invoice.html',context)
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Invoice_%s.pdf"%("12341231")
+            content = "inline; filename='%s'"%(filename)
+            download = request.GET.get("download")
+            if download:
+                content = "attachment; filename='%s'" %(filename)
+            response['Content-Disposition']=content
+            return response  
+
+        return HttpResponse("Not Found")
